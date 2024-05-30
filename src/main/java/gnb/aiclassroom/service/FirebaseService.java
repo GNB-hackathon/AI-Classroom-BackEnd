a872b9267f37ad1bf23a4f1bcc84a08a692c8011
vinyeee <loveyebin077@naver.com>
2024-05-31 01:04:07 +0900

    Google Firebase 연동

---
+++ src/main/java/gnb/aiclassroom/service/FirebaseService.java
@@ -0,0 +1,37 @@
+package gnb.aiclassroom.service;
+
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Bucket;
+import com.google.firebase.cloud.StorageClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+
+
+@Service
+public class FirebaseService {
+    private String firebaseBucket = "ai-classroom-50619.appspot.com";
+
+    public String uploadPhoto (MultipartFile file, String warehouseName) throws IOException {
+
+       //Logger logger = LoggerFactory.logger(FirebaseService.class);
+
+        Bucket bucket = StorageClient.getInstance().bucket(firebaseBucket);
+
+        // InputStream으로 파일 내용 읽기
+        try (InputStream content = file.getInputStream()) {
+            Blob blob = bucket.create(warehouseName, content, file.getContentType());
+
+            String url = blob.signUrl(365, TimeUnit.DAYS).toString();
+            //logger.info(url);
+
+
+            return url;
+        }
+
+    }
+
+}
